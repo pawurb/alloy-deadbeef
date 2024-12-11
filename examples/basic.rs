3f96331e440000ff651606757917b6d9a4c777e9
pawurb <contact@pawelurbanek.com>
2024-12-11 14:01:58 +0100

    sending real tx

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,4 +1,5 @@
 use alloy::{
+    eips::eip2718::Encodable2718,
     network::{EthereumWallet, TransactionBuilder},
     node_bindings::Anvil,
     primitives::address,
@@ -7,6 +8,7 @@ use alloy::{
     signers::local::PrivateKeySigner,
     uint,
 };
+use alloy_deadbeef::GWEI_I;
 use eyre::Result;
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -14,17 +16,18 @@ async fn main() -> Result<()> {
     let me = address!("82F8f740fD0B74ccDC7404dEe96fE1c9A9B7445C");
     // let rpc = "https://eth.merkle.io";
     let rpc = std::env::var("RPC_URL").unwrap();
-    // let provider = ProviderBuilder::new().on_http(rpc.parse()?);
+    let provider = ProviderBuilder::new().on_http(rpc.parse()?);
 
     let anvil = Anvil::new().fork(rpc).spawn();
 
     let signer: PrivateKeySigner = std::env::var("PRIVATE_KEY")?.parse()?;
     let wallet = EthereumWallet::from(signer);
     let anvil_provider = ProviderBuilder::new()
+        .with_chain_id(42161)
         // .wallet(wallet)
         .on_http(anvil.endpoint().parse()?);
 
-    let gas_price = anvil_provider.get_gas_price().await?;
+    let gas_price = provider.get_gas_price().await?;
     dbg!(gas_price);
     let nonce = anvil_provider.get_transaction_count(me).await?;
 
@@ -33,16 +36,18 @@ async fn main() -> Result<()> {
         to: Some(me.into()),
         value: Some(uint!(1_U256)),
         nonce: Some(nonce),
-        max_fee_per_gas: Some(gas_price),
-        max_priority_fee_per_gas: Some(gas_price / 10),
-        gas: Some(21000),
+        chain_id: Some(uint!(42161)),
+        max_fee_per_gas: Some(gas_price * 120 / 100),
+        max_priority_fee_per_gas: Some(GWEI_I),
+        gas: Some(210000),
         ..Default::default()
     };
     let tx_envelope = tx.build(&wallet).await?;
-    dbg!(tx_envelope);
+    let mut encoded_tx = vec![];
+    tx_envelope.encode_2718(&mut encoded_tx);
 
-    // let tx = anvil_provider.send_transaction(tx).await?;
-    // dbg!(tx);
+    let tx = provider.send_raw_transaction(&encoded_tx).await?;
+    dbg!(tx);
 
     Ok(())
 }
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+use alloy::{primitives::U256, uint};
+
+pub static ONE_ETHER: U256 = uint!(1_000_000_000_000_000_000_U256);
+pub static GWEI: U256 = uint!(1_000_000_000_U256);
+
+pub static GWEI_I: u128 = 1_000_000_000;
