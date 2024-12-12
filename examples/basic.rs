dc36baaad8540424c6ca17275a7ccc4095a3ccfb
pawurb <contact@pawelurbanek.com>
2024-12-12 13:59:03 +0100

    wip

--- examples/basic.rs
+++ examples/basic.rs
@@ -3,7 +3,7 @@ use alloy::{
     node_bindings::Anvil,
     primitives::{address, Address, U256},
     providers::{Provider, ProviderBuilder},
-    rpc::{client::ClientBuilder, types::TransactionRequest},
+    rpc::types::TransactionRequest,
     signers::local::PrivateKeySigner,
 };
 
@@ -17,19 +17,16 @@ async fn main() -> Result<()> {
     let provider = ProviderBuilder::new().on_http(rpc.parse()?);
 
     let anvil = Anvil::new().fork(rpc).spawn();
-
-    let client = ClientBuilder::default().http(anvil.endpoint().parse()?);
-
-    let anvil_provider = ProviderBuilder::new()
-        .with_chain_id(provider.get_chain_id().await?)
-        .on_client(client);
-
-    let gas_price = anvil_provider.get_gas_price().await?;
-    let nonce = anvil_provider.get_transaction_count(ME).await?;
-
     let signer: PrivateKeySigner = std::env::var("PRIVATE_KEY")?.parse()?;
     let wallet = EthereumWallet::from(signer);
     let chain_id = provider.get_chain_id().await?;
+
+    let anvil_provider = ProviderBuilder::new()
+        .wallet(wallet.clone())
+        .on_http(anvil.endpoint().parse()?);
+
+    let gas_price = anvil_provider.get_gas_price().await?;
+    let nonce = anvil_provider.get_transaction_count(ME).await?;
 
     let tx = TransactionRequest {
         from: Some(ME),
@@ -43,7 +40,14 @@ async fn main() -> Result<()> {
         ..Default::default()
     };
 
-    let res = prefixed_tx(tx, wallet, "deade").await?;
+    let res = prefixed_tx(tx, wallet, "dead").await?;
+
+    let res = anvil_provider
+        .send_transaction(res)
+        .await?
+        .get_receipt()
+        .await?;
+    dbg!(res.transaction_hash);
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,13 @@ use alloy::{
     uint,
 };
 use eyre::Result;
+use futures::future::select_all;
 use std::{sync::Arc, thread::available_parallelism};
-use tokio::sync::Notify;
+use tokio::sync::{Mutex, Notify};
 
 pub static ONE_ETHER: U256 = uint!(1_000_000_000_000_000_000_U256);
 pub static GWEI: U256 = uint!(1_000_000_000_U256);
 pub static GWEI_I: u128 = 1_000_000_000;
-
-pub struct PrefixResult {
-    pub tx: Option<TransactionRequest>,
-    pub iterations: u128,
-}
 
 pub async fn prefixed_tx(
     tx: TransactionRequest,
@@ -26,35 +22,32 @@ pub async fn prefixed_tx(
     let max_cores: u128 = available_parallelism().unwrap().get() as u128;
     dbg!(&max_cores);
     let max_value = 16_u128.pow(prefix.len() as u32);
+    let mut source_tx = tx.clone();
 
     let mut handles = vec![];
-
-    let done = Arc::new(Notify::new());
+    let done_guard = Arc::new(Mutex::new(false));
     for i in 0..max_cores {
         let tx = tx.clone();
         let prefix = prefix.to_string();
         let wallet = wallet.clone();
-        let done = done.clone();
+        let done_guard = done_guard.clone();
         let handle = tokio::spawn(async move {
-            search_tx_hash(
-                tx,
-                wallet,
-                i * max_value / max_cores,
-                prefix,
-                &i.to_string(),
-                done,
-            )
-            .await
-            .unwrap()
+            search_tx_hash(tx, wallet, i * max_value / max_cores, prefix, done_guard)
+                .await
+                .unwrap()
         });
         handles.push(handle);
     }
 
-    for handle in handles {
-        handle.await.unwrap();
-    }
+    let (res, _index, remaining) = select_all(handles).await;
 
-    todo!()
+    // for handle in remaining {
+    //     handle.abort();
+    // }
+
+    source_tx.value = res.unwrap();
+
+    Ok(source_tx)
 }
 
 async fn search_tx_hash(
@@ -62,44 +55,35 @@ async fn search_tx_hash(
     wallet: EthereumWallet,
     starting_input: u128,
     prefix: String,
-    label: &str,
-    done: Arc<Notify>,
-) -> Result<Option<FixedBytes<32>>> {
-    let mut iter = 0;
+    done_guard: Arc<Mutex<bool>>,
+) -> Result<Option<U256>> {
     let mut value = starting_input;
     let prefix = prefix.as_bytes();
 
-    let tx_hash: Option<FixedBytes<32>> = loop {
-        tokio::select! {
-            _ = done.notified() => {
-                break None;
-            }
-            else => {
-              let tx = tx.clone();
-              let next_value = tx.value.unwrap_or_default() + U256::from(value);
-              let tx_hash = calculate_hash(tx, &wallet, next_value).await?;
+    let result: Option<U256> = loop {
+        if *done_guard.lock().await {
+            dbg!("break");
+            break None;
+        }
+        let tx = tx.clone();
+        let next_value = tx.value.unwrap_or_default() + U256::from(value);
+        let tx_hash = calculate_hash(tx, &wallet, next_value).await?;
 
-              value += 1;
-              iter += 1;
+        value += 1;
 
-              if value % 10000 == 0 {
-                  dbg!(label, value, iter);
-              }
-
-              let hash_str = format!("{:x}", &tx_hash);
-              let hash_prefix = &hash_str[..prefix.len()];
-              let first_hash_bytes = hash_prefix.as_bytes();
-              if first_hash_bytes == prefix {
-                  dbg!("found");
-                  dbg!(hash_str);
-                  done.notify_one();
-                  break Some(tx_hash);
-              }
-            }
+        let hash_str = format!("{:x}", &tx_hash);
+        let hash_prefix = &hash_str[..prefix.len()];
+        let first_hash_bytes = hash_prefix.as_bytes();
+        if first_hash_bytes == prefix {
+            dbg!("found");
+            dbg!(hash_str);
+            let mut done = done_guard.lock().await;
+            *done = true;
+            break Some(next_value);
         }
     };
 
-    Ok(tx_hash)
+    Ok(result)
 }
 
 async fn calculate_hash(
